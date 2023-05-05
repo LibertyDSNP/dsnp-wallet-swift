bd93e8f59a53bd5581e710a4dfbb08b18b36474a
Ben G <bengabay89@gmail.com>
2023-05-05 15:26:20 -0400

    add step count, subtitle, desc, textfield, text binding, view model

--- DSNP-Wallet/Source/Common/ViewControllers/Onboarding/SignIn Screen/ClaimHandleView.swift
+++ DSNP-Wallet/Source/Common/ViewControllers/Onboarding/SignIn Screen/ClaimHandleView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct ClaimHandleView: View {
+    @ObservedObject var viewModel: ClaimHandleViewModel
+    
     var body: some View {
         VStack {
             AmplicaLogo()
@@ -26,12 +28,38 @@ struct ClaimHandleView: View {
             alignment: .top
         )
         .background(Color(uiColor: UIColor.Theme.bgTeal))
-
+    }
+    
+    private var stepCount: some View {
+        Text("1 of 3")
+            .font(Font(UIFont.Theme.regular(ofSize: 12)))
+            .foregroundColor(.black)
+    }
+    
+    private var subtitle: some View {
+        Text("Create your Digital Identity")
+            .font(Font(UIFont.Theme.regular(ofSize: 12)))
+            .foregroundColor(.black)
+    }
+    
+    private var description: some View {
+        Text("Your unique handle is how others will find you across\nthe social web and will be universal across apps.")
+            .font(Font(UIFont.Theme.regular(ofSize: 12)))
+            .foregroundColor(.black)
+    }
+    
+    private var textfield: some View {
+        VStack {
+            Text("Claim your handle")
+                .font(Font(UIFont.Theme.regular(ofSize: 12)))
+                .foregroundColor(.black)
+            TextField("", text: $viewModel.claimHandleText)
+        }
     }
 }
 
 struct ClaimHandleView_Previews: PreviewProvider {
     static var previews: some View {
-        ClaimHandleView()
+        ClaimHandleView(viewModel: ClaimHandleViewModel())
     }
 }
---
+++ DSNP-Wallet/Source/Common/ViewControllers/Onboarding/SignIn Screen/ClaimHandleViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ClaimHandleViewModel.swift
+//  DSNP-Wallet
+//
+//  Created by Ben Gabay on 5/5/23.
+//
+
+import UIKit
+
+class ClaimHandleViewModel: ObservableObject {
+    @Published var claimHandleText = ""
+
+}
