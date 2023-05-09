21f13b3d8a5a5d66415259f5485eec97659514f4
Ben G <bengabay89@gmail.com>
2023-05-09 10:19:59 -0400

    add new onboarding files

---
+++ DSNP-Wallet/Source/Modules/Onboarding/NewUI/Onboarding/CongratsModal.swift
@@ -0,0 +1,61 @@
+//
+//  CongratsModal.swift
+//  DSNP-Wallet
+//
+//  Created by Ben Gabay on 5/8/23.
+//
+
+import SwiftUI
+
+struct CongratsModal: View {
+
+    var body: some View {
+        VStack {
+            congratsText
+            description
+            buttonStack
+        }
+    }
+    
+    private var congratsText: some View {
+        Text("Congratulations!")
+            .font(Font(UIFont.Theme.bold(ofSize: 22)))
+            .foregroundColor(Color(uiColor: UIColor.Theme.congratsColor))
+    }
+    
+    private var buttonStack: some View {
+        VStack {
+            finishButton
+            skipButton
+        }
+    }
+    
+    private var description: some View {
+        Text("For better recommendations and\nto connect with people relevant to you filling\nout your profile is a good first step.")
+            .font(Font(UIFont.Theme.regular(ofSize: 22)))
+            .foregroundColor(.black)
+    }
+    
+    private var finishButton: some View {
+        PrimaryButton(title: "Finish Digital Identitys") {
+            // TODO: NEXT
+        }
+    }
+    
+    private var skipButton: some View {
+        Button {
+            // TODO SKIP
+        } label: {
+            Text("Skip for now (Not Recommended)")
+                .foregroundColor(Color(uiColor: UIColor.Theme.defaultTextColor))
+                .font(Font(UIFont.Theme.regular(ofSize: 10)))
+                .underline()
+        }
+    }
+}
+
+struct CongratsModal_Previews: PreviewProvider {
+    static var previews: some View {
+        CongratsModal()
+    }
+}
---
+++ DSNP-Wallet/Source/Modules/Onboarding/NewUI/Onboarding/SignInViewController.swift
@@ -0,0 +1,49 @@
+//
+//  SignInViewController.swift
+//  DSNP-Wallet
+//
+//  Created by Ben Gabay on 5/4/23.
+//
+
+import UIKit
+import SwiftUI
+import Combine
+
+class SignInViewController: UIHostingController<SignInView> {
+
+    let viewModel: SignInViewModel = SignInViewModel()
+    
+    private var cancellables = [AnyCancellable]()
+    
+    init() {
+        super.init(rootView: SignInView(viewModel: viewModel))
+    }
+    
+    @MainActor required dynamic init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupObservables()
+    }
+
+    private func setupObservables() {
+        viewModel.createIdentityAction.sink { [weak self] in
+            guard let self else { return }
+            self.navigationController?.pushViewController(ClaimHandleViewController(), animated: true)
+        }
+        .store(in: &cancellables)
+//        viewModel.meWeIdAction.sink { [weak self] in
+//            guard let self else { return }
+//
+//        }
+//        .store(in: &cancellables)
+//        viewModel.restoreAction.sink { [weak self] in
+//            guard let self else { return }
+//
+//        }
+//        .store(in: &cancellables)
+    }
+
+}
