dca4d5c3f1e985e5dca4ac45283588aa066d1f10
Ben G <bengabay89@gmail.com>
2023-05-15 10:00:42 -0400

    seed phrase view work - wip

--- DSNP-Wallet/Source/Modules/Onboarding/SeedPhrase/SwiftUI/SeedPhraseView.swift
+++ DSNP-Wallet/Source/Modules/Onboarding/SeedPhrase/SwiftUI/SeedPhraseView.swift
@@ -15,30 +15,55 @@ struct SeedPhraseView: View {
             testHeadline
             testDescription
             confirmText
-            SeedView(words: ["Hello", "Hello2"])
+            SeedView()
             writtenDownButton
         }
+        .background(Color(uiColor: UIColor.Theme.bgTeal))
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
     
     private var headline: some View {
-        Text("Headline")
+        Text("Headline goes here")
+            .foregroundColor(.white)
+            .font(Font(UIFont.Theme.bold(ofSize: 22)))
+            .padding(.horizontal, 16)
+            .padding(.vertical, 8)
+            .frame(alignment: .leading)
     }
     
     private var headlineSubtitle: some View {
-        Text("subtitle")
+        Text("Back up your recovery phrase now to ensure that your assets will be stores safely and securely")
+            .foregroundColor(.white)
+            .font(Font(UIFont.Theme.regular(ofSize: 12)))
+            .padding(.horizontal, 16)
+    }
+    
+    private var testDescription: some View {
+        Text("Please carefully write down these 12 words below in numerical order (1-12) and store your secret phrase securely. You will confirm them in order on the next screen.")
+            .foregroundColor(.white)
+            .font(Font(UIFont.Theme.regular(ofSize: 12)))
+            .padding(.horizontal, 16)
+            .padding(.vertical, 8)
+            .frame(alignment: .leading)
 
     }
     
     private var testHeadline: some View {
-        Text("test Headline")
-    }
-    
-    private var testDescription: some View {
-        Text("test description")
+        Text("There will be a test")
+            .foregroundColor(.white)
+            .font(Font(UIFont.Theme.bold(ofSize: 22)))
+            .padding(.horizontal, 16)
+            .padding(.vertical, 8)
+            .frame(alignment: .leading)
     }
     
     private var confirmText: some View {
-        Text("confirm")
+        Text("You will confirm them in order on the next screen")
+            .foregroundColor(.white)
+            .font(Font(UIFont.Theme.regular(ofSize: 12)))
+            .padding(.horizontal, 16)
+            .padding(.vertical, 8)
+            .frame(alignment: .leading)
     }
     
     private var writtenDownButton: some View {
@@ -53,11 +78,30 @@ struct SeedPhraseView: View {
 
 struct SeedView: View {
     
-    let words: [String]
+    let words: [String] = ["Cat","Dog","Rat","Hamster","Iguana","Newt"]
+    
+    let columns: [GridItem] =
+    Array(repeating: .init(.fixed(12)), count: 2)
+
+    
+    var body: some View {
+        HStack {
+            SeedPhraseColumnView()
+            SeedPhraseColumnView()
+        }
+    }
+}
+
+struct SeedPhraseColumnView: View {
+    let words: [String] = ["Cat","Dog","Rat","Hamster","Iguana","Newt"]
     
     var body: some View {
         VStack {
-            
+            ForEach(words, id: \.self) { word in
+                Text(word)
+                    .foregroundColor(.white)
+                    .font(Font(UIFont.Theme.regular(ofSize: 12)))
+            }
         }
     }
 }
