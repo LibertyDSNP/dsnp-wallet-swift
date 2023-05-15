cc24d66c0e91fa5f0630472798ea88b5933a596a
Ben G <bengabay89@gmail.com>
2023-05-15 10:22:25 -0400

    fix seed phrase view

--- DSNP-Wallet/Source/Modules/Onboarding/SeedPhrase/SwiftUI/SeedPhraseView.swift
+++ DSNP-Wallet/Source/Modules/Onboarding/SeedPhrase/SwiftUI/SeedPhraseView.swift
@@ -17,9 +17,12 @@ struct SeedPhraseView: View {
             confirmText
             SeedView()
             writtenDownButton
+            Spacer()
         }
+        .padding(.top, 80)
         .background(Color(uiColor: UIColor.Theme.bgTeal))
         .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .ignoresSafeArea()
     }
     
     private var headline: some View {
@@ -58,20 +61,31 @@ struct SeedPhraseView: View {
     }
     
     private var confirmText: some View {
-        Text("You will confirm them in order on the next screen")
-            .foregroundColor(.white)
-            .font(Font(UIFont.Theme.regular(ofSize: 12)))
-            .padding(.horizontal, 16)
-            .padding(.vertical, 8)
-            .frame(alignment: .leading)
+        HStack {
+            Text("You will confirm them ")
+                .foregroundColor(.white)
+                .font(Font(UIFont.Theme.regular(ofSize: 12)))
+                .padding(.horizontal, -8)
+            Text("in order")
+                .foregroundColor(Color(uiColor: UIColor.Theme.textOrange))
+                .font(Font(UIFont.Theme.bold(ofSize: 12)))
+                .frame(alignment: .leading)
+            Text(" on the next screen")
+                .foregroundColor(.white)
+                .font(Font(UIFont.Theme.regular(ofSize: 12)))
+                .padding(.horizontal, -8)
+        }
+        .padding(.horizontal, 16)
+        .padding(.vertical, 8)
+
     }
     
     private var writtenDownButton: some View {
-        Button {
-            
-        } label: {
-            Text("I've written it down")
+        PrimaryButton(title: "I've written it down") {
+            // ACTION
         }
+        .padding(.horizontal, 20)
+        .padding(.vertical, 30)
     }
     
 }
@@ -80,15 +94,13 @@ struct SeedView: View {
     
     let words: [String] = ["Cat","Dog","Rat","Hamster","Iguana","Newt"]
     
-    let columns: [GridItem] =
-    Array(repeating: .init(.fixed(12)), count: 2)
-
-    
     var body: some View {
         HStack {
             SeedPhraseColumnView()
             SeedPhraseColumnView()
         }
+        .padding(.leading, 30)
+        .frame(maxWidth: .infinity, alignment: .center)
     }
 }
 
@@ -97,11 +109,46 @@ struct SeedPhraseColumnView: View {
     
     var body: some View {
         VStack {
-            ForEach(words, id: \.self) { word in
-                Text(word)
+            ForEach(Array(words.enumerated()), id: \.element) { index, element in
+                Text("\(SeedPhraseHelper.textForIndex(index: index)) \(element)")
                     .foregroundColor(.white)
-                    .font(Font(UIFont.Theme.regular(ofSize: 12)))
+                    .font(Font(UIFont.Theme.spaceRegular(ofSize: 15)))
+                    .frame(minWidth: 0, maxWidth: 100, minHeight: 0, maxHeight: 20, alignment: .leading)
+
             }
+        }
+    }
+}
+
+struct SeedPhraseHelper {
+    static func textForIndex(index: Int) -> String {
+        switch index {
+        case 0:
+            return "01"
+        case 1:
+            return "02"
+        case 2:
+            return "03"
+        case 3:
+            return "04"
+        case 4:
+            return "05"
+        case 5:
+            return "06"
+        case 6:
+            return "07"
+        case 7:
+            return "08"
+        case 8:
+            return "09"
+        case 9:
+            return "10"
+        case 10:
+            return "11"
+        case 11:
+            return "12"
+        default:
+            return "00"
         }
     }
 }
