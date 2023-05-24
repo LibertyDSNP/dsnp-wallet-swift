de75037015ed7f752ec0035bfd9f42294ef6a354
Ben G <bengabay89@gmail.com>
2023-05-24 10:28:58 -0400

    extract terms disclaimer view to its own file

--- DSNP-Wallet/Source/Modules/Onboarding/NewUI/AgreeToTermsView.swift
+++ DSNP-Wallet/Source/Modules/Onboarding/NewUI/AgreeToTermsView.swift
@@ -21,7 +21,7 @@ struct AgreeToTermsView: View {
                 agreementText
                 agreementExplanationText
                 agreeButton
-                termsView
+                TermsDisclaimerView()
                     .padding(.top, 120)
             }
             .frame(maxWidth: .infinity, maxHeight: .infinity)
@@ -93,50 +93,6 @@ struct AgreeToTermsView: View {
             viewModel.agreeAction.send()
         }
     }
-    
-    var termsView: some View {
-        VStack {
-            Text("By signing up, you agree to Amplica Access")
-                .font(Font(UIFont.Theme.regular(ofSize: 12)))
-                .foregroundColor(Color(uiColor: UIColor.Theme.termsTextColor))
-                .lineSpacing(3)
-                .frame(maxHeight: .infinity, alignment: .bottom)
-            if #available(iOS 16.0, *) {
-                HStack {
-                    
-                    Text("[Terms](https://google.com)")
-                        .accentColor(Color(uiColor: UIColor.Theme.termsTextColor))
-                        .font(Font(UIFont.Theme.regular(ofSize: 12)))
-                        .underline()
-                        .padding(.horizontal, -4)
-                    Text("and")
-                        .accentColor(Color(uiColor: UIColor.Theme.termsTextColor))
-                        .font(Font(UIFont.Theme.regular(ofSize: 12)))
-                        .foregroundColor(Color(uiColor: UIColor.Theme.termsTextColor))
-                    Text("[Privacy Policy](https://google.com)")
-                        .accentColor(Color(uiColor: UIColor.Theme.termsTextColor))
-                        .font(Font(UIFont.Theme.regular(ofSize: 12)))
-                        .underline()
-                        .padding(.horizontal, -4)
-                    Text("•")
-                        .accentColor(Color(uiColor: UIColor.Theme.termsTextColor))
-                        .font(Font(UIFont.Theme.regular(ofSize: 12)))
-                        .foregroundColor(Color(uiColor: UIColor.Theme.termsTextColor))
-                    Text("[Learn More](https://google.com)")
-                        .accentColor(Color(uiColor: UIColor.Theme.termsTextColor))
-                        .font(Font(UIFont.Theme.regular(ofSize: 12)))
-                        .underline()
-                }
-            } else {
-                Text("By signing up, you agree to our [Terms](https://google.com) and\n[Privacy Policy](https://google.com) • [Learn More](https://google.com)")
-                    .font(Font(UIFont.Theme.regular(ofSize: 12)))
-                    .foregroundColor(.black)
-                    .lineSpacing(3)
-            }
-        }
-        
-    }
-    
 }
 
 struct AgreeToTermsView_Previews: PreviewProvider {
