f88f7a961cf895975516bd21c650f0beb1046042
Ben G <bengabay89@gmail.com>
2023-06-01 11:33:17 -0400

    create component for tab item, utilize it. Fix image inconsistencies

--- DSNP-Wallet/Source/Common/TabBar/HomeView.swift
+++ DSNP-Wallet/Source/Common/TabBar/HomeView.swift
@@ -11,38 +11,31 @@ struct HomeTabView: View {
     
     let viewModel: HomeViewModel
     
+    @State public var tabViewSelection = 0
+    
     @State var showingAlert = false
     
     var body: some View {
-        TabView {
+        TabView(selection: $tabViewSelection) {
             AMPProfileView(viewModel: AMPHomeViewModel(), showingAlert: showingAlert)
                 .tabItem {
-                    VStack {
-                        Text("Home")
-                        Image("Home")
-                    }
+                    AmpTabItem(title: "Home", tabImageName: "home", tabSelectedImageName: "home_selected", isSelected: tabViewSelection == 0)
                 }
+                .tag(0)
             ProfileView()
                 .tabItem {
-                    VStack {
-                        Text("Profile")
-                        Image("Profile")
-                    }
-                }
+                    AmpTabItem(title: "Profile", tabImageName: "profile", tabSelectedImageName: "profile_selected", isSelected: tabViewSelection == 1)                }
+                .tag(1)
             PermissionsView()
                 .tabItem {
-                    VStack {
-                        Text("Settings")
-                        Image("Keys")
-                    }
+                    AmpTabItem(title: "permissions", tabImageName: "permissions", tabSelectedImageName: "permissions_selected", isSelected: tabViewSelection == 2)
                 }
+                .tag(2)
             SettingsView()
                 .tabItem {
-                    VStack {
-                        Text("Settings")
-                        Image("settings")
-                    }
+                    AmpTabItem(title: "Settings", tabImageName: "settings", tabSelectedImageName:  "settings_selected", isSelected: tabViewSelection == 3)
                 }
+                .tag(3)
         }
         .accentColor(Color(uiColor: UIColor.Theme.primaryTeal))
         .background(Color(uiColor: UIColor.Theme.bgTeal))
@@ -80,6 +73,27 @@ struct PermissionsView: View {
     var body: some View {
         VStack {
             
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .ignoresSafeArea()
+        .background(Color(uiColor: UIColor.Theme.bgTeal))
+    }
+}
+
+struct AmpTabItem: View {
+    
+    let title: String
+    let tabImageName: String
+    let tabSelectedImageName: String
+    
+    @State var isSelected: Bool = false
+    
+    var body: some View {
+        VStack {
+            Text(title)
+                .foregroundColor(isSelected ? Color(uiColor: UIColor.Theme.primaryTeal) : .white)
+            Image(isSelected ? tabSelectedImageName : tabImageName)
+                .frame(maxWidth: 20, maxHeight: 20, alignment: .center)
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .ignoresSafeArea()
