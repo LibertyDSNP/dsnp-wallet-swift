2ed1c793284e2d1bb7901ac343315aa6fcc95848
Ryan Sheh <ryan.sheh@unfinished.com>
2023-06-01 15:43:51 -0700

    Updated conversion logic to accurately handle little endian hex to get appropriate error index

--- DSNP-Wallet/Source/Common/Substrate/Types/RuntimeDispatchError.swift
+++ DSNP-Wallet/Source/Common/Substrate/Types/RuntimeDispatchError.swift
@@ -9,25 +9,37 @@ import Foundation
 import SubstrateSdk
 import BigInt
 
+enum DecodingDispatchError: Error {
+    case badConversion
+}
+
 struct RuntimeDispatchError: Decodable {
     private let array: [JSON]
     let index: UInt8?
     let error: UInt8?
-    
+
+    //We get error index in little endian, hexademical.
+    //Review: Big Endian -> |12| |34| |56| |78|, Little Endian |78| |56| |34| |12|
+    //e.g.:
+    // index 2 -> hex is 0002 (4 bytes) -> ["2", "0", "0", "0"]
+    // index 17 -> hex is 11 -> ["11", "0", "0", "0"]
     init(from decoder: Decoder) throws {
         var unkeyedContainer = try decoder.unkeyedContainer()
         array = try unkeyedContainer.decode([JSON].self)
-        
-        let errorDict = array.last?.dictValue as? [String: JSON]
-        index = UInt8(errorDict?["index"]?.stringValue ?? "")
-        
-        let errorIndices = errorDict?["error"]?.arrayValue as? [JSON] ?? []
-        let errorArray: [String] = errorIndices.compactMap { return $0.stringValue }
-        let hexString = errorArray.joined(separator: "")
 
-        let byteArray = try Data(hexString: hexString) //e.g. convert 0x0400 into 4
+        let errorDict = array.last?.dictValue as? [String: JSON] //.last bc .first of DispatchError is just string "Module"
+        index = UInt8(errorDict?["index"]?.stringValue ?? "")
+
+        let errorIndices = errorDict?["error"]?.arrayValue as? [JSON] ?? []
+        let errorArray: [String] = try errorIndices.compactMap { jsonVal in
+            guard var indexStringVal = jsonVal.stringValue else { throw DecodingDispatchError.badConversion }
+            return indexStringVal.count == 1 ? "0" + indexStringVal : indexStringVal
+        }
+        let errorString = errorArray.joined(separator: "")
+        let byteArray = try Data(hexString: errorString)
         let errorIndex = byteArray.withUnsafeBytes { $0.load(as: UInt16.self) }
-        error = UInt8(errorIndex )
+
+        error = UInt8(errorIndex)
     }
 }
 
