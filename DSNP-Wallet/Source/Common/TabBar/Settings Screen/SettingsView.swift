100323c59d810f9a5c6743e1223842ec097dd87c
Ben G <bengabay89@gmail.com>
2023-06-06 10:38:15 -0400

    add settings components, wip

--- DSNP-Wallet/Source/Common/TabBar/Settings Screen/SettingsView.swift
+++ DSNP-Wallet/Source/Common/TabBar/Settings Screen/SettingsView.swift
@@ -15,6 +15,9 @@ struct SettingsView: View {
         VStack {
             headline
             recoverySection
+            security
+            faceIdCell
+            password
             logoutButton
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
@@ -24,13 +27,19 @@ struct SettingsView: View {
     
     private var headline: some View {
         Text("Settings")
+            .font(Font(UIFont.Theme.regular(ofSize: 16)))
+            .foregroundColor(.white)
     }
     
     private var recoverySection: some View {
         VStack {
             if !AppState.shared.hasBackedKeys {
                 Text("You Haven't backed up")
+                    .font(Font(UIFont.Theme.thin(ofSize: 12)))
+                    .foregroundColor(.white)
                 Text("Back it up")
+                    .font(Font(UIFont.Theme.thin(ofSize: 12)))
+                    .foregroundColor(.white)
             }
             PrimaryButton(title: "Reveal Recovery Phrase") {
                 // TODO: Navigate to seed flow
@@ -41,19 +50,42 @@ struct SettingsView: View {
     private var security: some View {
         VStack {
             Text("Security")
+                .font(Font(UIFont.Theme.bold(ofSize: 14)))
+                .foregroundColor(.white)
             Text("Adding additional security is really important")
+                .font(Font(UIFont.Theme.thin(ofSize: 12)))
+                .foregroundColor(.white)
         }
     }
     
     private var faceIdCell: some View {
         HStack {
             VStack {
-                
+                Text("Face ID")
+                    .font(Font(UIFont.Theme.bold(ofSize: 14)))
+                    .foregroundColor(.white)
+                Text("Increase access security with Face ID")
+                    .font(Font(UIFont.Theme.thin(ofSize: 12)))
+                    .foregroundColor(.white)
             }
-            
+            Toggle("Increase access security with Face ID", isOn: .constant(true))
         }
     }
     
+    private var password: some View {
+        HStack {
+            VStack {
+                Text("Password")
+                    .font(Font(UIFont.Theme.bold(ofSize: 14)))
+                    .foregroundColor(.white)
+                Text("Password to log in to your account")
+                    .font(Font(UIFont.Theme.thin(ofSize: 12)))
+                    .foregroundColor(.white)
+            }
+            Spacer()
+            // arrow
+        }
+    }
     
     private var logoutButton: some View {
         Button("log out") {
